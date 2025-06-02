50d4d9af1b7dd6ac4403d4d19ea5f12fdad9ed88
Nikolas Batsaris <74042221+nikolasbatsaris@users.noreply.github.com>
2025-06-02 15:09:08 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
-set "url=https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-windows_x64.zip"
+set "url=https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x64.zip"
 set "zip=%TEMP%\rustdesk.zip"
 set "dir=%TEMP%\rustdesk"
 
-curl -L %url% -o %zip%
+bitsadmin /transfer "RustDeskDL" %url% %zip%
 powershell -Command "Expand-Archive -Path '%zip%' -DestinationPath '%dir%' -Force"
 start "" "%dir%\rustdesk\rustdesk.exe"
